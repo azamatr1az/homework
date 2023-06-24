2346b4f13cf9ca870a914cbd68d7c88a5a1c95e5
Azamat <38736017+azamatr1az@users.noreply.github.com>
2023-06-25 00:59:38 +0200

    Update ExtraCalories1.java

--- ExtraCalories1.java
+++ ExtraCalories1.java
@@ -22,8 +22,48 @@ public class ExtraCalories1 {
     }
 }
 
+//Экземпляр класса Extracalories1
+
+package Homework;
+
+public class Extracalories2 {
+    public static void main(String[] args) {
+        int radius1 = 12; // Радиус первой пиццы (24 см / 2)
+        int radius2 = 14; // Радиус второй пиццы (28 см / 2)
+        int caloriesPerSquareCm = 40; // Количество калорий на каждый квадратный сантиметр
+
+        ExtraCalories1 a1 = new ExtraCalories1(radius1, radius2, caloriesPerSquareCm);
+
+    }
+}    
+
+//здесь еще один способ с использованием scannera
+
+package Homework;
+import java.util.Scanner;
+
+public class ExtraCalories {
+    public static void  main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Extracal extra = new Extracal();
+        scanner.close();
+    }
+    static class  Extracal {
+        public Extracal() {
+
+            int radius1 = 12; // Радиус первой пиццы (24 см / 2)
+            int radius2 = 14; // Радиус второй пиццы (28 см / 2)
+            int caloriesPerSquareCm = 40; // Количество калорий на каждый квадратный сантиметр
+
+            double area1 = Math.PI * (radius1 * radius1); // Площадь первой пиццы
+            double area2 = Math.PI * (radius2 * radius2); // Площадь второй пиццы
+            double extraCalories = (area2 - area1) * caloriesPerSquareCm; // Лишние калории
+
+            System.out.println("При замене пиццы диаметром 24 см на пиццу диаметром 28 см будет " + extraCalories + " лишних калорий.");
 
 
-
+        }
+    }
+}
 
 
