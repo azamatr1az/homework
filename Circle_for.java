4768f707035903737026c142620b92600d6e9a89
Azamat <38736017+azamatr1az@users.noreply.github.com>
2023-07-01 21:18:27 +0200

    Add files via upload

---
+++ Circle_for.java
@@ -0,0 +1,10 @@
+package Homework;
+
+public class Circle_for {
+    public static void main(String[] args) {
+        for (int i = 0, number = 1; i < 10; i++) {
+            System.out.print(number + " ");
+            number *= 2;
+        }
+    }
+}
